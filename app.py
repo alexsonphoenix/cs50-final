31f9fc36a11681b528ea2bb60df9e6080eb308e4
Alexsonphoenix <alexsonphoenix@gmail.com>
2019-10-22 23:44:25 +1300

    working on flask_bootstrap and wtforms. However, experiencing an error: CSRF tokens do not match which raised when trying to use form.csrf_token to avoid CSRF web failure

--- app.py
+++ app.py
@@ -8,14 +8,31 @@ from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
 from fractions import Fraction
+from flask_wtf import RecaptchaField, FlaskForm
+from wtforms import StringField, DateTimeField, PasswordField
+from wtforms.validators import InputRequired, Length
+from flask_bootstrap import Bootstrap
+from flask_wtf.csrf import CSRFProtect
 
 from helpers import apology, login_required
 
+
+
 # Configure application
 app = Flask(__name__)
+Bootstrap(app)
+
+
+# CSRF uses this key to prevent forms being submitted by people not on the route
+app.config['SECRET_KEY'] = '911fe2e60c2e152b08adbf0b9432bed0b2dd4003'
 
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Recaptcha by Google to ensure robots don't mess with the database
+app.config["RECAPTCHA_PUBLIC_KEY"] = '6Le30b4UAAAAAOm6ssR1O8_utiAVzll8fvT-5Osq'
+app.config["RECAPTCHA_PRIVATE_KEY"] = '6Le30b4UAAAAAGsIMurKZ7TPCijfGR0A9kld7LCG'
+app.config["TESTING"] = True
 
 # Ensure responses aren't cached
 @app.after_request
@@ -24,7 +41,6 @@ def after_request(response):
     response.headers["Expires"] = 0
     response.headers["Pragma"] = "no-cache"
     return response
-
 
 # Configure session to use filesystem (instead of signed cookies(default))
 app.config["SESSION_FILE_DIR"] = mkdtemp()
@@ -45,14 +61,20 @@ def index():
     return render_template("index.html")
 
 
+# Form for login
+class LoginForm(FlaskForm):
+    username = StringField('username', validators=[InputRequired()])
+    password = PasswordField('password', validators=[InputRequired()])
+
 @app.route("/login", methods=["GET", "POST"])
 def login():
     """Log user in"""
     # Forget any user_id
     session.clear()
-
+    form = LoginForm()
+    print(f'csrf_token: ' + session['csrf_token'])
     # User reached route via POST (as by submitting a form via POST)
-    if request.method == "POST":
+    if form.validate_on_submit():
         # Ensure username was submitted
         if not request.form.get("username"):
             return apology("must provide username", 403)
@@ -77,7 +99,7 @@ def login():
 
     # User reached route via GET (as by clicking a link or via redirect)
     else:
-        return render_template("login.html")
+        return render_template("login.html", form = form)
 
 
 @app.route("/logout")
@@ -148,13 +170,21 @@ def register():
     else:
         return render_template("register.html")
 
+
+# Form for setting route
+class settingForm(FlaskForm):
+    trackingYears = StringField('trackingYears')
+    crop = StringField('crop')
+
+
 @app.route("/setting", methods=["GET", "POST"])
 @login_required
 def setting():
     """Setting Page."""
+    form = settingForm()
     # User reached route via GET (as by clicking a link or via redirect)
     if request.method == "GET":
-        return render_template("setting.html")
+        return render_template("setting.html", form=form)
 
     # User reached route via GET (as by clicking a link or via redirect)
 
